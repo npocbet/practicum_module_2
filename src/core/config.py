967c3c6665b7d31fa91e8b2254d3beac7175da4d
alexbravada <sc2alex@yandex.ru>
2022-10-15 12:09:54 +0000

    add pydantic settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,10 @@
+from dataclasses import Field
 import os
 from logging import config as logging_config
+from fastapi.responses import ORJSONResponse
 
 from core.logger import LOGGING
-
+from pydantic import BaseSettings, RedisDsn
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
@@ -22,3 +24,39 @@ FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5  # 5 минут
 
 # Корень проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Settings(BaseSettings):
+    debug: bool = False
+    docs_url: str = '/doc'
+    openapi_prefix: str = ''
+    openapi_url: str = '/openapi.json'
+    redoc_url: str = '/redoc'
+    title: str = 'FastAPI Sprint 1 module 2'
+    version: str = "1.0"
+    default_response_class = ORJSONResponse
+
+    redis_uri: RedisDsn = 'redis://127.0.0.1:6379'
+    REDIS_HOST: str = Field('127.0.0.1', env='REDIS_HOST')
+    REDIS_PORT: str = Field('6379', env='REDIS_PORT')
+    elastic_uri: str = ''
+    ELASTIC_HOST: str = Field('127.0.0.1', env='ELASTIC_HOST')
+    ELADTIC_PORT: int = Field(9200, env='ELASTIC_PORT')
+
+    allowed_hosts: List[str] = ["*"]
+
+    class Config:
+         env_file = '.env'
+
+    @property
+    def fastapi_kwargs(self) -> Dict[str, Any]:
+        return {
+            "debug": self.debug,
+            "docs_url": self.docs_url,
+            "openapi_prefix": self.openapi_prefix,
+            "openapi_url": self.openapi_url,
+            "redoc_url": self.redoc_url,
+            "title": self.title,
+            "version": self.version,
+            "default_response_class": self.default_response_class,
+        }
--- src/main.py
+++ src/main.py
@@ -11,21 +11,16 @@ from core import config
 from core.logger import LOGGING
 from db import elastic, redis
 
+settings = config.Settings()
 
-app = FastAPI(
-    title=config.PROJECT_NAME,
-    docs_url='/api/openapi',
-    openapi_url='/api/openapi.json',
-    default_response_class=ORJSONResponse,
-)
-
+app = FastAPI(**settings.fastapi_kwargs)
 
 @app.on_event('startup')
 async def startup():
     redis.redis = await aioredis.create_redis_pool(
-        (config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20
+        (settings.REDIS_HOST, settings.REDIS_PORT), minsize=10, maxsize=20
     )
-    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    elastic.es = AsyncElasticsearch(hosts=[f'{settings.ELASTIC_HOST}:{settings.ELADTIC_PORT}'])
 
 
 @app.on_event('shutdown')
